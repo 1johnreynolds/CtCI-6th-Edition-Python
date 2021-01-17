559fb8f2b18da441c44e566b6466ea6f9cb1e275
John Reynolds <68815686+1johnreynolds@users.noreply.github.com>
2021-01-17 09:22:12 -0700

    Update p04_palindrome_permutation.py
    Added function that checks string for palindrome permutation without using counter module.

--- chapter_01/p04_palindrome_permutation.py
+++ chapter_01/p04_palindrome_permutation.py
@@ -39,6 +39,18 @@ def is_palindrome_permutation_pythonic(phrase):
     counter = Counter(phrase.replace(" ", "").lower())
     return sum(val % 2 for val in counter.values()) <= 1
 
+def is_palindrome_permutation_no_counter(phrase):
+  """function checks if a string is a permutation of a palindrome or not"""
+  phrase = phrase.replace(" ", "").lower()
+  char_list = [0]*256
+  counter = 0
+
+  for char in phrase:
+    char_list[ord(char)] += 1
+  for val in char_list:
+    counter += val % 2
+    
+  return (counter <= 1)
 
 class Test(unittest.TestCase):
     test_cases = [
@@ -56,7 +68,7 @@ class Test(unittest.TestCase):
         ("no x in nixon", True),
         ("azAZ", True),
     ]
-    testable_functions = [is_palindrome_permutation, is_palindrome_permutation_pythonic]
+    testable_functions = [is_palindrome_permutation, is_palindrome_permutation_pythonic, is_palindrome_permutation_no_counter]
 
     def test_pal_perm(self):
         for f in self.testable_functions:
